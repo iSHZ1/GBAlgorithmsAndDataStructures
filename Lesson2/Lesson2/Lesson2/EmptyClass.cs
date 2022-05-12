97f2d9af68b07f509c32f338df82531430ee89a2
IMacSHZ <Xardas366@gmail.com>
2022-05-12 21:10:23 +0300

    start

---
+++ Lesson2/Lesson2/Lesson2/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Lesson2
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+}
---
+++ Lesson2/Lesson2/Lesson2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            List<Node> ArrayNode = new List<Node>();
+            bool ProgramStart = true;
+
+            while(ProgramStart)
+            {
+
+                ArrayNode.Add(ProgramInput());
+                if (true)
+                {
+
+                }
+            }
+
+        }
+
+        public Node ProgramInput()
+        {
+            Console.WriteLine("Если хотите добавить экземпляр в массив? Y/N");
+            Char n = Convert.ToChar(Console.ReadKey());
+
+
+            if (n =='Y' || n == 'y')
+            {
+                Console.WriteLine("введите значение Нода");
+                return new Node();
+            }
+            if (n == 'N' || n == 'n')
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Повторите попытку ввода");
+            }
+        }
+
+    }
+}
